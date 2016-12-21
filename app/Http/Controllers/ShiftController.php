33cd010fc759557dd3b697d97ee517c8f254c3c3
Ryan Basnet <basnetrahzes@hotmail.com>
2016-12-22 02:17:16 +1100

    SHiftController added

---
+++ app/Http/Controllers/ShiftController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Shift;
+use Carbon\Carbon;
+class ShiftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "all shift";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $startTime = Carbon::createFromFormat('H:i',$request->json('startTime'));
+        $endTime = Carbon::createFromFormat('H:i',$request->json('endTime'));
+        $minutes = $endTime->diffInMinutes($startTime);
+        $hours=date('H:i', mktime(0,$minutes));
+        // echo $hours;
+        // exit;
+        $shift = Shift::create([
+
+                'start_time'=>$startTime,
+                'end_time'=>$endTime,
+                'hours'=>$hours,
+                'schedule_id'=>$request->json('scheduleId')
+
+
+
+            ])->id;
+
+        return response()->json(Shift::find($shift));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return "show";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "update";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return "deleted";
+    }
+}
